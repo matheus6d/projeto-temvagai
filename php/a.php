ee9952f345b85fe52d46b09977370df602835bf3
Amanda Oliveira <67129120+amandaoliveira1@users.noreply.github.com>
2020-12-04 16:13:45 -0300

    Add files via upload

---
+++ php/a.php
@@ -0,0 +1,22 @@
+<?php include('conexão.php');
+
+   
+        if (!empty($titulo) || !empty($descricao) || !empty($valor) || !empty($categoria) ) {
+        
+             $sql = "UPDATE dados SET titulo= $titulo WHERE ID=$id";
+             $sql = "UPDATE dados SET descricao= $descricao WHERE ID=$id";
+             $sql = "UPDATE dados SET valor= $valor WHERE ID=$id";
+            
+            if ($conn->query($sql) === TRUE) {
+                echo "";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+        $conn->close();
+
+        }
+
+
+
+    ?>
